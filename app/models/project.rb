580fef2ae073be723372070dff91d2d370124d0d
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-13 18:03:01 -0700

    Add project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
-    
+    has_many :tasks
+
+    validates :title, :creator_id, :due_date, presence: true
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
     has_many :assignments
     has_many :users, through: :assignments
     belongs_to :creator, class_name: "User"
+    belongs_to :project
 
     validates :title, :due_date, :creator_id, presence: true
 
