b9add5fbf342d9fea32d149685469b74c18fd815
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-13 18:30:17 -0700

    Create tasks controller

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,48 @@
 class Api::TasksController < ApplicationController
+
+
+    def index
+        render json: Task.all
+    end
+
+    def create
+        task = Task.create!(task_params)
+        render json: task, status: :created
+    end
+
+    def show
+        task = find_task
+        render json: task
+    end
+
+    def update
+        task = find_task
+        task.update!(task_params)
+        render json: task
+    end
+
+    def destroy
+        task = find_task
+        event.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def task_params
+        params.permit(
+            :title,
+            :notes,
+            :due_date,
+            :creator_id,
+            :project_id,
+        )
+    end
+
+    def find_task
+        Task.find(params[:id])
+    end
+
+
 end
