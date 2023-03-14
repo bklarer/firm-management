8cbf8978fbf21f1b57ddf5f4020b307cbb99be6e
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-13 17:58:35 -0700

    Fix task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
+    has_many :assignments
+    has_many :users, through: :assignments
+    belongs_to :creator, class_name: "User"
+
+    validates :title, :due_date, :creator_id, presence: true
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     has_many :tasks, through: :assignments
 
     has_many :created_tasks, class_name: "Task", foreign_key: "creator_id"
+    has_many :created_projects, class_name: "Project", foreign_key: "creator_id"
 
     validates :username, presence: true, uniqueness: true
     validates :first_name, :last_name, presence: true
