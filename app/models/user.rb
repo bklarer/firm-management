73a0f6e269810aad5b1fc4df30cd65192bb3d474
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-13 17:49:07 -0700

    Build user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :assignments
+    has_many :tasks, through: :assignments
+
+    has_many :created_tasks, class_name: "Task", foreign_key: "creator_id"
+
+    validates :username, presence: true, uniqueness: true
+    validates :first_name, :last_name, presence: true
 end
