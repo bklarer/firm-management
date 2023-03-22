127a2e6cc6b21605a43db17eb1ecdf94e32757fb
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-22 10:24:08 -0700

    Add error handling

--- client/src/slices/loginSlice.js
+++ client/src/slices/loginSlice.js
@@ -1,131 +1,127 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { createAsyncThunk } from "@reduxjs/toolkit"
-
+import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const headers = {
-    "content-type": "application/json",
-    Accept: "application/json",
-  };
-  
-  export const registerUser = createAsyncThunk(
-    `login/signup`,
-    async (data, { rejectWithValue }) => {
-      const response = await fetch("/api/signup", {
-        method: "POST",
-        headers,
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        const error = await response.json();
-        return rejectWithValue(error.errors);
-      }
-  
+  "content-type": "application/json",
+  Accept: "application/json",
+};
+
+export const registerUser = createAsyncThunk(
+  `login/signup`,
+  async (data, { rejectWithValue }) => {
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return rejectWithValue(error.errors);
+    }
+
+    const user = await response.json();
+    return user;
+  }
+);
+
+export const userLogin = createAsyncThunk(
+  `login/login`,
+  async (data, { rejectWithValue }) => {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      return rejectWithValue(error.errors);
+    }
+
+    const user = await response.json();
+    console.log(user);
+    return user;
+  }
+);
+
+export const checkLogin = createAsyncThunk(
+  `login/check`,
+  async ({ rejectWithValue }) => {
+    const response = await fetch("/api/me");
+
+    if (response.ok) {
       const user = await response.json();
       return user;
+    } else {
+      const error = await response.json();
+      return rejectWithValue(error.errors);
     }
-  );
-  
-  export const userLogin = createAsyncThunk(
-    `login/login`,
-    async (data, { rejectWithValue }) => {
-      const response = await fetch("/api/login", {
-        method: "POST",
-        headers,
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        const error = await response.json();
-        return rejectWithValue(error.errors);
-      }
-  
-      const user = await response.json();
-      console.log(user)
-      return user;
-    }
-  );
-  
-  export const checkLogin = createAsyncThunk(
-    `login/check`,
-    async ({rejectWithValue}) => {
-      const response = await fetch("/api/me");
-  
-      if (!response.ok) {
-        const error = await response.json();
-        return rejectWithValue(error.errors);
-      }
-  
-      const user = await response.json();
-      return user;
-    }
-  );
-  
+  }
+);
 
 const initialState = {
-    loading: false,
-    userInfo: null,
-    error: null,
-  };
-  
-  const loginSlice = createSlice({
-    name: "login",
-    initialState,
-    reducers: {
-      logout: (state) => {
+  loading: false,
+  userInfo: null,
+  error: null,
+};
+
+const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.loading = false;
+      state.userInfo = null;
+      state.error = null;
+    },
+    clearUserError: (state) => {
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(userLogin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(userLogin.fulfilled, (state, { payload }) => {
         state.loading = false;
-        state.userInfo = null;
+        state.userInfo = payload;
         state.error = null;
-      },
-      clearUserError: (state) => {
+      })
+      .addCase(userLogin.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
         state.error = null;
-      },
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(userLogin.pending, (state) => {
-          state.loading = true;
-          state.error = null;
-        })
-        .addCase(userLogin.fulfilled, (state, { payload }) => {
-          state.loading = false;
-          state.userInfo = payload;
-          state.error = null;
-        })
-        .addCase(userLogin.rejected, (state, { payload }) => {
-          state.loading = false;
-          state.error = payload;
-        })
-        .addCase(registerUser.pending, (state) => {
-          state.loading = true;
-          state.error = null;
-        })
-        .addCase(registerUser.fulfilled, (state, { payload }) => {
-          state.loading = false;
-          state.userInfo = payload;
-          state.error = null;
-        })
-        .addCase(registerUser.rejected, (state, { payload }) => {
-          state.loading = false;
-          state.error = payload;
-        })
-        .addCase(checkLogin.pending, (state) => {
-          state.loading = true;
-          state.error = null;
-        })
-        .addCase(checkLogin.fulfilled, (state, { payload }) => {
-          state.loading = false;
-          state.userInfo = payload;
-        })
-        .addCase(checkLogin.rejected, (state, { payload }) => {
-          state.loading = false;
-          state.error = payload;
-        });
-    },
-  });
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.userInfo = payload;
+        state.error = null;
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(checkLogin.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(checkLogin.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.userInfo = payload;
+      })
+      .addCase(checkLogin.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      });
+  },
+});
 
-  export const {
-    logout
-  } = loginSlice.actions;
+export const { logout } = loginSlice.actions;
 
-  export default loginSlice.reducer;
+export default loginSlice.reducer;
--- client/src/slices/userSlice.js
+++ client/src/slices/userSlice.js
@@ -1,43 +1,49 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async ({ rejectWithValue }) => {
     const response = await fetch("/api/users");
-    const users = await response.json();
-    return users;
-  });
 
- //check if waiting for promise
+    if (response.ok) {
+      const users = await response.json();
+      return users;
+    } else {
+      const error = await response.json();
+      return rejectWithValue(error.errors);
+    }
+  }
+);
+
+//reject with value
 
 const initialState = {
-    users: [],
-    loading: false,
-    error: null,
-   };
+  users: [],
+  loading: false,
+  error: null,
+};
 
-   const usersSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchUsers.pending, (state) => {
-          state.loading = true;
-        })
-        .addCase(fetchUsers.fulfilled, (state, action) => {
-          state.users = action.payload;
-          state.loading = false;
-        })
-        .addCase(fetchUsers.rejected, (state, action) => {
-          state.error = action.error.message;
-          state.loading = false;
-        });
-    },
-  });
-  
-  
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.users = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.error = action.error.message;
+        state.loading = false;
+      });
+  },
+});
+
 //   export const {
 //   } = usersSlice.actions;
-  
-  
-  export default usersSlice.reducer;
+
+export default usersSlice.reducer;
