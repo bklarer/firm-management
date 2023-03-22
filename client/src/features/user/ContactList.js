06ae94e395720a6fb558a4f6d6fe2b99c0a76b63
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-22 08:47:43 -0700

    Show firm as a contact with number of tasks

--- client/src/features/user/ContactList.js
+++ client/src/features/user/ContactList.js
@@ -6,8 +6,10 @@ import { useSelector } from "react-redux";
 const ContactList = () => {
     const contacts = useSelector((state) => state.users.users);
     const { userInfo } = useSelector((state) => state.login);
+    const tasks = useSelector((state) => state.tasks.tasks);
     const currentUser = contacts.length > 0 ? contacts.find((contact) => contact.id === userInfo.id) : {}
     const filteredContacts = contacts.length > 0 ? contacts.filter((contact) => contact.id !== userInfo.id) : []
+
 
     return(
 
@@ -17,6 +19,10 @@ const ContactList = () => {
                 <h4 className="name">Name</h4>
                 <h4>Tasks</h4>
             </div>
+        <div className="contact">
+            <h4 className="name">Firm's Tasks</h4>
+            <h4 className="to-do">{tasks.length}</h4>
+        </div>
             <Contact contact={currentUser}/>
             {filteredContacts.length > 0 ? filteredContacts.map((contact) => {
                 return (
