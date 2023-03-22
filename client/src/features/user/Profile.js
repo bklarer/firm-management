7def565dd989a844048f4f083084a8a2eb516d4a
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-21 21:16:38 -0700

    Connect user object to profile

--- client/src/features/user/Profile.js
+++ client/src/features/user/Profile.js
@@ -1,18 +1,18 @@
-
+import { useSelector, useDispatch } from "react-redux";
 
 
 
 const Profile = () => {
-
+    const { userInfo } = useSelector((state) => state.login);
 
 
     return(
 
         <div>
             <p>Picture</p>
-            <p>Username</p>
-            <p>First Name + Last Name</p>
-            <p>Email</p>
+            <p>{`Username: ${userInfo.username}`}</p>
+            <p>{`Name: ${userInfo.first_name} ${userInfo.last_name}`}</p>
+            <p>{`email: ${userInfo.email}`}</p>
         </div>
     )
 }
