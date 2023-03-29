f2b46ca44b44226d52d62e7220a0bb54e4cffdc8
Benjamin Klarer <benjaminklarer@gmail.com>
2023-03-28 19:08:24 -0700

    Test 2

--- client/src/App.js
+++ client/src/App.js
@@ -59,7 +59,7 @@ function App() {
           <Navbar />
           {!userInfo ? (
             <Routes>
-              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<SignUp />} />
               {/* <Route path="*" element={<Login />} /> */}
 
