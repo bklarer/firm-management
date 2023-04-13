3e4df41bfb3e4e9acbc4fcafecb95aed58fb166d
Benjamin Klarer <benjaminklarer@gmail.com>
2023-04-12 19:28:38 -0700

    Create email message

--- app/mailers/assignment_mailer.rb
+++ app/mailers/assignment_mailer.rb
@@ -1,9 +1,8 @@
 class AssignmentMailer < ApplicationMailer
     def new_assignment_email
         @assignment = params[:assignment]
-        # @task = @assignment.task
-        # @user = @assignment.user
-        # @creator = @task.creator
+        @task = @assignment.task
+        @user = @assignment.user
         mail(to: 'uncle.brk@gmail.com', subject: 'New Assignment')
     end
 
