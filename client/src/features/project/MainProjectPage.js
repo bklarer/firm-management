7dc121a7be8b42bd3f82ff4ecd81a6103f5837ef
Benjamin Klarer <benjaminklarer@gmail.com>
2023-04-17 22:34:36 -0700

    Setup auto navigate when landing on project page

--- client/src/features/project/MainProjectPage.js
+++ client/src/features/project/MainProjectPage.js
@@ -1,11 +1,20 @@
-
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const MainProjectPage = () => {
+    const projects = useSelector((state) => state.projects.projects);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        if (projects.length > 0) {
+            navigate(`/projects/${projects[0].id}`);
+        }
+    }, [projects, navigate]);
 
     return (
         <div>
-            Select a project
+            Create a new project to show projects
         </div>
 
     )
