09568f2da85ce722058c88ecd18a1209bcb0dd5b
Benjamin Klarer <benjaminklarer@gmail.com>
2023-04-24 19:40:39 -0700

    Add ability to collapse left-bar

--- client/src/features/structure/LeftBar.js
+++ client/src/features/structure/LeftBar.js
@@ -1,22 +1,33 @@
-import { Outlet } from "react-router-dom"
-import ContactList from "../user/ContactList"
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react"
+import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
 
 const LeftBar = () => {
+  const [isHidden, setIsHidden] = useState(false);
 
+  const handleClick = () => {
+    setIsHidden(!isHidden);
+  };
 
+  console.log("isHidden", isHidden);
 
-    return (
+  return (
+    
+    <div
+      className={`left-bar ${isHidden ? "hidden" : ""}`}
+    >
+      <div className="menu-icon" >
+        
+        <svg onClick={handleClick} viewBox="0 0 100 80" width="40" height="40">
+          <rect width="100" height="20"></rect>
+          <rect y="30" width="100" height="20"></rect>
+          <rect y="60" width="100" height="20"></rect>
+        </svg>
+      </div>
+      <div className={isHidden ? "hide-content" : ""}>
+      <Outlet />
+      </div>
+    </div>
+  );
+};
 
-        <div className="left-bar">
-            <Outlet/>
-        </div>
-
-
-    )
-
-
-}
-
-export default LeftBar
+export default LeftBar;
