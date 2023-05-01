759d9b9ae87b4e4677166b47cefd3a513a46e598
Benjamin Klarer <benjaminklarer@gmail.com>
2023-04-30 22:23:32 -0700

    Adjust project list

--- client/src/features/project/ProjectList.js
+++ client/src/features/project/ProjectList.js
@@ -1,41 +1,47 @@
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import {dateHelper} from "../../helpers/dateTime";
+import { dateHelper } from "../../helpers/dateTime";
 
 const ProjectList = () => {
   const projects = useSelector((state) => state.projects.projects);
   const [dropdown, setDropdown] = useState("all");
 
-
-  let date = new Date().toISOString()
+  let date = new Date().toISOString();
 
   const filteredProjects = () => {
-    let newProjects = []
-    if(projects.length > 0 ) {
-        switch(dropdown) {
-            case "not_completed":
-                newProjects = projects.filter((project) => project.completed === false)
-            break;
-            case "past_due":
-                newProjects = projects.filter((project) => project.completed === false && project.due_date < date)
-            break;
-            case "completed":
-                newProjects = projects.filter((project) => project.completed === true)
-            break;
-            default: 
-            newProjects = projects
-        }
+    let newProjects = [];
+    if (projects.length > 0) {
+      switch (dropdown) {
+        case "not_completed":
+          newProjects = projects.filter(
+            (project) => project.completed === false
+          );
+          break;
+        case "past_due":
+          newProjects = projects.filter(
+            (project) => project.completed === false && project.due_date < date
+          );
+          break;
+        case "completed":
+          newProjects = projects.filter(
+            (project) => project.completed === true
+          );
+          break;
+        default:
+          newProjects = projects;
+      }
     }
-    return newProjects
-}
-
+    return newProjects;
+  };
 
   return (
     <>
       <div className="project-filter">
-      <select onChange={(e) => setDropdown(e.target.value)} value={dropdown}>
-          <option default value="all">All</option>
+        <select onChange={(e) => setDropdown(e.target.value)} value={dropdown}>
+          <option default value="all">
+            All
+          </option>
           <option value="not_completed">Not complete</option>
           <option value="past_due">Past Due</option>
           <option value="completed">Completed</option>
