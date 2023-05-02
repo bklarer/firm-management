3c4cc950af009d7f4e9496203d60ef0a2a4f7735
Benjamin Klarer <benjaminklarer@gmail.com>
2023-05-01 20:59:42 -0700

    Fix profile when no image is uploaded.

--- client/src/features/user/Profile.js
+++ client/src/features/user/Profile.js
@@ -4,17 +4,29 @@ import { Link } from "react-router-dom";
 const Profile = () => {
   const { userInfo } = useSelector((state) => state.login);
 
+  const addDefaultSrc = (e) => {
+    e.target.src =
+      "https://res.cloudinary.com/dnahj1ggn/image/upload/v1681614097/face_sktddp.jpg";
+  };
+
+  const imagePlaceholder =
+    "https://res.cloudinary.com/dnahj1ggn/image/upload/v1681614097/face_sktddp.jpg";
+
   return (
     <div className="profile">
       <h2>Profile</h2>
-      <img src={userInfo.image} alt="Profile" style={{ width: "200px" }} />
+      <img
+        onError={addDefaultSrc}
+        src={userInfo.image ? userInfo.image : imagePlaceholder}
+        alt="Profile"
+        style={{ width: "200px" }}
+      />
       <p>{`Username: ${userInfo.username}`}</p>
       <p>{`Name: ${userInfo.first_name} ${userInfo.last_name}`}</p>
       <p>{`email: ${userInfo.email}`}</p>
-        <Link className="link" to="edit">
-          Edit
-        </Link>
-
+      <Link className="link" to="edit">
+        Edit
+      </Link>
     </div>
   );
 };
